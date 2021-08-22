f2f8967507904a1b09f5cdd71a08f24fa8ead8d1
Davide Falcone <davidefalcone777@gmail.com>
2021-08-22 12:58:00 +0200

    added login state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:custom_barber_shop/screens/home_screen.dart';
 import 'package:custom_barber_shop/state/state_management.dart';
+import 'package:custom_barber_shop/utils/utils.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_auth_ui/firebase_auth_ui.dart';
 import 'package:firebase_auth_ui/providers.dart';
@@ -6,6 +8,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/all.dart';
+import 'package:page_transition/page_transition.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,6 +22,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      onGenerateRoute: (settings){
+        switch(settings.name){
+          case '/home':
+            return PageTransition(
+                settings: settings,
+                child: HomePage(),
+                type: PageTransitionType.fade);
+                break;
+          default: return null;
+        }
+      },
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -31,77 +45,31 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class MyHomePage extends ConsumerWidget {
 
   GlobalKey<ScaffoldState> scaffoldState = new GlobalKey();
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldState,
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/my_bg.png'),
-            fit: BoxFit.cover
-          )
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(16),
-              width: MediaQuery.of(context).size.width,
-              child: ElevatedButton.icon(
-                onPressed: () => processLogin(context),
-                icon: Icon(Icons.phone, color: Colors.white,),
-                label: Text('LOGIN CON NUMERO', style: TextStyle(color: Colors.white),),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.black)
-                ),
-              )
-            )
-          ],
-        ),
-      ),
-// This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
 
-  processLogin(BuildContext context) {
+  //called when we login (not every time we open the app)
+  processLogin(BuildContext context) async{
     var user = FirebaseAuth.instance.currentUser;
     if(user == null){
       FirebaseAuthUi.instance()
           .launchAuth([
             AuthProvider.phone()
-      ]).then((firebaseUser){
+      ]).then((firebaseUser) async {
           //refresh state
         context.read(userLogged).state = FirebaseAuth.instance.currentUser;
-        ScaffoldMessenger.of(scaffoldState.currentContext).showSnackBar
-          (SnackBar(content: Text('Login avvenuto con successo ${FirebaseAuth.instance.currentUser.phoneNumber}')));
+        //start new screen
+        await checkLoginState(context, true);
+
       }).catchError((ex){
         if(ex is PlatformException){
           if(ex.code == FirebaseAuthUi.kUserCancelledError){
@@ -116,5 +84,71 @@ class _MyHomePageState extends State<MyHomePage> {
     }else{
 
     }
+  }
+
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    return Scaffold(
+      key: scaffoldState,
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/my_bg.png'),
+                fit: BoxFit.cover
+            )
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+                padding: const EdgeInsets.all(16),
+                width: MediaQuery.of(context).size.width,
+                child: FutureBuilder(
+                  future: checkLoginState(context, false),
+                  builder: (context, snapshot){
+                    if(snapshot.connectionState == ConnectionState.waiting){
+                      return Center(child: CircularProgressIndicator(),);
+                    }
+                    else{
+                      var userState = snapshot.data as LOGIN_STATE;
+                      if(userState == LOGIN_STATE.LOGGED){
+                        return Container();
+                      }else{ //if not logged
+                        return ElevatedButton.icon(
+                          onPressed: ()=> processLogin(context),
+                          icon: Icon(Icons.phone, color:Colors.white),
+                          label: Text('LOGIN CON TELEFONO', style: TextStyle(color: Colors.white),),
+                          style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.black)),
+                        );
+                      }
+                    }
+
+                  }
+
+                )
+            )
+          ],
+        ),
+      ),
+// This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  //called every start of application
+  Future<LOGIN_STATE> checkLoginState(BuildContext context, bool fromLogin) async{
+    await Future.delayed(Duration(seconds: fromLogin == true? 0 : 3))
+        .then((value) => {
+      FirebaseAuth.instance.currentUser
+          .getIdToken()
+          .then((token){
+            //if the token is true, we get it and print it
+            print('$token');
+            context.read(userToken).state = token;
+            //if we got here, we are already logged, we shot the hjome screen
+            Navigator.pushNamedAndRemoveUntil(context, '/home', (route) => false);
+      })
+    });
+
+    return FirebaseAuth.instance.currentUser != null ? LOGIN_STATE.LOGGED : LOGIN_STATE.NOT_LOGGED;
   }
 }

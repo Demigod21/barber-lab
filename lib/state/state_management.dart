f960c9cf499a69ea288aea9a0d80ee463c1f9754
davide <davide.falcone276@gmail.com>
2021-08-25 07:54:37 +0200

    added profile update

--- lib/state/state_management.dart
+++ lib/state/state_management.dart
@@ -5,3 +5,4 @@ import 'package:hooks_riverpod/all.dart';
 
 final userLogged = StateProvider((ref) => FirebaseAuth.instance.currentUser);
 final userToken = StateProvider((ref) => '');
+final forceReload = StateProvider((ref) => false);
